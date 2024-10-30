c5858926b1545991302429b5c02b024c66353720
Alban Bailly <abailly@akamai.com>
2024-10-30 17:42:29 -0400

    xFilter util improvements

--- packages/manager/src/features/Volumes/VolumesLanding.tsx
+++ packages/manager/src/features/Volumes/VolumesLanding.tsx
@@ -71,14 +71,11 @@ export const VolumesLanding = () => {
     preferenceKey,
   });
 
-  // const filter: Filter = {
-  //   ['+order']: order,
-  //   ['+order_by']: orderBy,
-  //   ...(query && {
-  //     label: { '+contains': query },
-  //   }),
-  // };
-  const volumeXFilters = buildVolumeXFilters(query);
+  const volumeXFilters = buildVolumeXFilters({
+    order,
+    orderBy,
+    query,
+  });
   const { data: volumes, error, isFetching, isLoading } = useVolumesQuery(
     {
       page: pagination.page,
