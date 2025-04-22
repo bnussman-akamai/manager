e4936a7166e20971b83c24d5eaf8815a292f5188
Banks Nussman <banks@nussman.us>
2025-04-22 15:13:54 -0400

    clean up unassign dialog mutation

--- packages/manager/src/features/Linodes/LinodesDetail/LinodeNetworking/LinodeInterfaces/UnassignFirewallConfirmationDialog.tsx
+++ packages/manager/src/features/Linodes/LinodesDetail/LinodeNetworking/LinodeInterfaces/UnassignFirewallConfirmationDialog.tsx
@@ -4,8 +4,8 @@ import {
   useLinodeInterfaceQuery,
   useRemoveFirewallDeviceMutation,
 } from '@linode/queries';
-import { Button, Typography } from '@linode/ui';
-import { useQueryClient } from '@tanstack/react-query';
+import { Button, Stack, Typography } from '@linode/ui';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { useSnackbar } from 'notistack';
 import React from 'react';
 
@@ -13,6 +13,8 @@ import { Code } from 'src/components/Code/Code';
 import { ConfirmationDialog } from 'src/components/ConfirmationDialog/ConfirmationDialog';
 
 import { getLinodeInterfaceType } from './utilities';
+
+import type { APIError, Firewall } from '@linode/api-v4';
 
 interface Props {
   interfaceId: number | undefined;
@@ -39,59 +41,82 @@ export const UnassignFirewallConfirmationDialog = (props: Props) => {
     interfaceId !== undefined
   );
 
-  const { mutateAsync: deleteDevice, isPending } = useRemoveFirewallDeviceMutation();
+  const { mutateAsync: deleteDevice } = useRemoveFirewallDeviceMutation();
 
   const firewall = firewalls?.data[0];
 
   const interfaceType =
     linodeInterface && getLinodeInterfaceType(linodeInterface);
 
-  const onUnassign = async () => {
-    if (!firewall) {
-      return enqueueSnackbar('Unable to load Firewalls for this Interface.', {
-        variant: 'error',
-      });
-    }
-
-    const devices = await queryClient.ensureQueryData(
-      firewallQueries.firewall(firewall.id)._ctx.devices
-    );
-
-    const device = devices.find(
-      (device) =>
-        device.entity.id === interfaceId && device.entity.type === 'interface'
-    );
-
-    if (!device) {
-      return enqueueSnackbar(
-        'Unable to find Interface in the Firewall devices.',
-        {
-          variant: 'error',
-        }
-      );
-    }
-
-    await deleteDevice({ firewallId: firewall.id, deviceId: device.id });
-    enqueueSnackbar(
-      `Firewall ${firewall.label} has been unassined from your ${interfaceType} interface.`,
-      {
-        variant: 'success',
-      }
-    );
+  const handleClose = () => {
+    reset();
     onClose();
   };
+
+  const {
+    mutate: onUnassign,
+    error,
+    isPending,
+    reset,
+  } = useMutation<Firewall, APIError[]>({
+    async mutationFn() {
+      if (!firewall) {
+        return Promise.reject([
+          {
+            reason: "Unable to determine this Interface's Firewall. Refresh and try again.",
+          },
+        ]);
+      }
+
+      const devices = await queryClient.ensureQueryData(
+        firewallQueries.firewall(firewall.id)._ctx.devices
+      );
+
+      const device = devices.find(
+        (device) =>
+          device.entity.id === interfaceId && device.entity.type === 'interface'
+      );
+
+      if (!device) {
+        return Promise.reject([
+          {
+            reason:
+              "Interface does not appear to be a device on this Interface's Firewall. Refresh and try again.",
+          },
+        ]);
+      }
+
+      await deleteDevice({ firewallId: firewall.id, deviceId: device.id });
+
+      return firewall;
+    },
+    onSuccess(firewall) {
+      enqueueSnackbar(
+        `Firewall ${firewall.label} has been unassigned from your ${interfaceType} interface.`,
+        {
+          variant: 'success',
+        }
+      );
+      handleClose();
+    },
+  });
 
   return (
     <ConfirmationDialog
       actions={
-        <>
-          <Button onClick={onClose}>Cancel</Button>
-          <Button buttonType="primary" loading={isPending} onClick={onUnassign}>
+        <Stack direction="row" pt={1}>
+          <Button onClick={handleClose}>Cancel</Button>
+          <Button
+            buttonType="primary"
+            loading={isPending}
+            onClick={() => onUnassign()}
+          >
             Unassign
           </Button>
-        </>
+        </Stack>
       }
-      onClose={onClose}
+      error={error?.[0].reason}
+      onClose={handleClose}
       open={open}
       title="Unassign Firewall?"
     >
