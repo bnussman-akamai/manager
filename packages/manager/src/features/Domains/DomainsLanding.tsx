4f06ce99a0629766d98c646d09862454776f1670
Alban Bailly <abailly@akamai.com>
2025-05-18 22:34:08 -0400

    More work on the modal and calls

--- packages/manager/src/features/Domains/DomainsLanding.tsx
+++ packages/manager/src/features/Domains/DomainsLanding.tsx
@@ -52,6 +52,7 @@ import { EditDomainDrawer } from './EditDomainDrawer';
 
 import type { Handlers as DomainHandlers } from './DomainActionMenu';
 import type { Domain } from '@linode/api-v4';
+import type { DomainState } from 'src/routes/domains';
 
 const DOMAIN_CREATE_ROUTE = '/domains/create';
 
@@ -68,7 +69,7 @@ export const DomainsLanding = (props: DomainsLandingProps) => {
   const match = useMatch({
     strict: false,
   });
-  const locationState = location.state;
+  const locationState = location.state as DomainState;
 
   const { enqueueSnackbar } = useSnackbar();
   const { data: profile } = useProfile();
